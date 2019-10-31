f3bfd4750890b5051bba93123afaae5503ddbcef
E. Camilo <elvis.camilo@mosek.com>
2019-10-31 16:31:27 +0100

    Added test file; Added requirements file; Updated README.md;

---
+++ Events/tests/test_passenger_decision_event.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestPassengerDecisionEvent(unittest.TestCase):
+    """
+    A class to test passenger events.
+    """
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
